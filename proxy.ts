765fffbf8522fc16b26363425342b41ce00b558a
yubrani <yag24001@byui.edu>
2026-01-24 01:13:59 -0300

    database commit login

---
+++ proxy.ts
@@ -0,0 +1,10 @@
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+
+const { auth } = NextAuth(authConfig);
+
+export default auth;
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
