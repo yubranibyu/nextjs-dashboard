7333d7d4a5a91ffb8d7cc5228a9cc7554440c64f
yubrani <yag24001@byui.edu>
2026-01-30 01:37:22 -0300

    asdasd

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -14,7 +14,7 @@ import { useSearchParams } from 'next/navigation';
  
 export default function LoginForm() {
   const searchParams = useSearchParams();
-  const callbackUrl = searchParams.get('callbackUrl') || '/dashboard';
+  const callbackUrl = searchParams.get('callbackUrl') || '/dashboard/overview';
   const [errorMessage, formAction, isPending] = useActionState(
     authenticate,
     undefined,
